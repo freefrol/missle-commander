8f0bb0176e7b6be85a09cc0da4b47fb976a6a877
freefrol <kradaiver@mail.ru>
2026-08-11 19:01:41 +0300

    Last Sync: 2026-08-11 19:01 (Mobile)

--- Script/game_manager.gd
+++ Script/game_manager.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 @export var shootControllerScene : PackedScene
+@export var spawnEnemyRocketsScene : PackedScene
+
 var shootController
+var spawnEnemyRockets 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	shootController = shootControllerScene.instantiate()
+	spawnEnemyRockets = spawnEnemyRocketsScene.instantiate()
+	
+	add_child(shootController)
+	add_child(spawnEnemyRockets)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
